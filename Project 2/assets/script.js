d659bd733e35253d8cd538a69a12d1125d6d4205
bpoehlitz13 <bpoehlitz13@wou.edu>
2018-02-22 08:52:49 -0800

    lots of updates

--- Project 2/assets/script.js
+++ Project 2/assets/script.js
@@ -1,6 +1,12 @@
-$('button').click(function(){
-	$('.image').fadeToggle(12000);
+$(document).ready(function() {
+	alert('welcome :)');
 });
-$('button').click(function(){
-	$('.vanish').hide();
+$(function() {
+    $('.cathover').hide();
+    $('.catsleep').mouseenter(function() {
+      	$('.cathover').show();
+    });
+    $('.cathover').mouseleave(function() {
+      	$('.cathover').hide();
+    });
 });

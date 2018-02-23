c076f6bce1502ee423013e48bab3c9db56230f79
bpoehlitz13 <bpoehlitz13@wou.edu>
2018-02-23 12:46:07 -0800

    add it

--- Project 2/assets/script.js
+++ Project 2/assets/script.js
@@ -5,8 +5,10 @@ $(function() {
     $('.cathover').hide();
     $('.catsleep').mouseenter(function() {
       	$('.cathover').show();
+      	$('body').css("background-color", "firebrick");
     });
     $('.cathover').mouseleave(function() {
       	$('.cathover').hide();
+      	$('body').css("background-color", "white");
     });
 });

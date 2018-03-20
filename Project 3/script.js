0224d1c33651d548033b0d12de29c9a4e44d7251
bpoehlitz13 <bpoehlitz13@wou.edu>
2018-03-20 15:07:05 -0700

    push it up

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,5 +1,5 @@
 $('.about').click(function() {
-	alert("My name is Bryce Poehlitz. I draw a lot of different things. This is a sample of the things I like to draw.");
+	alert("My name is Bryce Poehlitz. I draw a lot of different things. This is a sample of the many things I like to draw.");
 });
 $('.button-toggler').click(function(){
     $('.button-container').fadeToggle(200);

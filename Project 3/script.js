f789033a77cef850735131f6fa5226cceba1c3b3
bpoehlitz13 <bpoehlitz13@wou.edu>
2018-03-21 11:39:34 -0700

    push

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,5 +1,5 @@
 $('.about').click(function() {
-	alert("My name is Bryce Poehlitz. I draw a lot of different things. This is a sample of the many things I like to draw.");
+	alert("My name is Bryce Poehlitz. I draw a lot of different things. This is only a sample of the many things I like to draw. If you need to contact me for any reason, my email address is bphltz@gmail.com.");
 });
 $('.button-toggler').click(function(){
     $('.button-container').fadeToggle(200);
